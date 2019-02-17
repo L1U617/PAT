5f0ab3c1423d6fad567518e78e2d9bb1aaef5551
Tsing <l875383221@gmail.com>
2019-02-17 13:29:05 +0800

    Update 1029.cpp

--- 1029.cpp
+++ 1029.cpp
@@ -11,17 +11,16 @@ int main()
 {
 	int n1, n2;
 	cin >> n1;
-	vector<int> s1(n1 + 1);
+	vector<int> s1(n1);
 	for (int i = 0; i < n1; i++) 
 		scanf("%d", &s1[i]);
-	s1[n1] = 0x7fffffff; //加了就通过，不加就不通过……
 	cin >> n2;
 	int cnt = 0, mid = (n1 + n2 - 1) / 2, j = 0;
 	int temp;
 	int flag = 0;
 	for (int i = 0; i < n2; i++) {
 		scanf("%d", &temp);
-		while (temp > s1[j]) {
+		while (temp > s1[j] && j < n1) {
 			if (cnt == mid) {
 				cout << s1[j];
 				flag = 1;
